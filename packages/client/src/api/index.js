088a112da35c2ab2ab03e11da9e40c78371de2e9
Valkema <usagiserenity16@gmail.com>
2021-06-03 15:47:28 +0300

    first commit

---
+++ packages/client/src/api/index.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const apiInstance = axios.create({
+	baseURL: "http://localhost:3000/api",
+});
---
+++ packages/server/router.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+//const someNameRouter = require("./routers/someNameRouter");
+const router = Router();
+
+module.exports = router;
